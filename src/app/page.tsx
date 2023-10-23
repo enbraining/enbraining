dcfd0ea7e7370a0964e9abdcd255d1a0cacdb426
Donghak Kim <6283hz@Donghaks-MacBook-Pro.local>
2023-10-23 17:55:16 +0900

    update padding & margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="rounded-md shadow-md">
-        <p className="p-30">김동학</p>
+        <p className="p-10 m-10">김동학</p>
       </div>
     </main>
   )
