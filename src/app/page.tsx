0c662cd7a27226aa90ad8317bfcf1859e3398b21
Donghak Kim <6283hz@Donghaks-MacBook-Pro.local>
2023-10-23 17:40:54 +0900

    delete boader size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <div className="grid place-items-center align-middle outline-2">
         <a className="p-30">김동학</a>
       </div>
