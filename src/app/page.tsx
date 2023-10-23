ab6db6bab302dff21786305c064a0efabe6d2937
Donghak Kim <6283hz@Donghaks-MacBook-Pro.local>
2023-10-23 17:35:49 +0900

    update 김동학

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="grid place-items-center">
-
+        <a className="p-30">김동학</a>
       </div>
     </main>
   )
