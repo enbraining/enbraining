93b1bf0298d44c04609fb3f774ae38b5e2818356
Donghak Kim <6283hz@Donghaks-MacBook-Pro.local>
2023-10-23 17:42:05 +0900

    update what

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div className="grid place-items-center align-middle outline-2">
+      <div className="grid place-items-center outline-2">
         <a className="p-30">김동학</a>
       </div>
     </main>
