72ac5b52c5e4e37658ff6e7042022bcaec190715
Donghak Kim <6283hz@Donghaks-MacBook-Pro.local>
2023-10-23 18:16:48 +0900

    asdf

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/6283hz/Documents/6283hz/6283hz/src/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="rounded-md shadow-md">
-        <p className="mx-10 my-5">김동학</p>
+      <div className="rounded-md shadow-md pt-10 pr-10 pb-10">
+        <p className="mx-10 mt-5 text-2xl">김동학</p>
+        <p className="mx-10 mb-5 text-lg">Backend Developer</p>
       </div>
     </main>
   )
