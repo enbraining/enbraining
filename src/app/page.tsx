6fa2a017c0d1fb9ba95a1a79064cee3d783a650c
Donghak Kim <6283hz@Donghaks-MacBook-Pro.local>
2023-10-23 17:51:19 +0900

    i dont know

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="border-4 border-indigo-200 border-t-indigo-500">
+      <div className=" border-indigo-200 border-2 rounded-md">
         <p className="p-30">김동학</p>
       </div>
     </main>
