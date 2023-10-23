597853eb54f818967afd3ebc6ec26d536e876150
Donghak Kim <6283hz@Donghaks-MacBook-Pro.local>
2023-10-23 17:25:53 +0900

    update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,61 +18,6 @@ export default function Home() {
             오픈소스 강의 서비스
           </p>
         </a>
-
-        <a
-          href="https://discord.gg/36tCw3kYuJ"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Discord{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            개발자들의 커뮤니티
-          </p>
-        </a>
-
-        <a
-          href="https://github.com/xolving"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Qilte{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          비영리 오픈소스 개발
-          </p>
-        </a>
-
-        <a
-          href="https://github.com/xolving"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Aerofunc{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            다양한 컴퓨팅 서비스
-          </p>
-        </a>
-      </div>
-
-      <div className='text-center'>
-        <img src='../../public/image.png'/>
       </div>
     </main>
   )
